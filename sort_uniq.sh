ee35f63aadd2bca0334d671d8dac27bd3043e3d8
jagankumar2001 <154983877+jagankumar2001@users.noreply.github.com>
2024-03-11 07:25:58 +0530

    Create sort_uniq.sh
    All the sort and uniq options are available which we use in our day to day activities. Sometimes the uniq command will yield wrong result so it is recommended to use sort and than use uniq command

---
+++ sort_uniq.sh
@@ -0,0 +1,15 @@
+Syntax:
+      sort filename
+
+sort -r filename     ->Sort file in reverse order
+
+sort -K2 filename      -> Sort by field number
+
+Syntax:
+      uniq
+
+sort filename  | uniq    -> Sort the file and deleted the duplicates
+
+sort filename | uniq -c    -> Sorts the file and removed the duplicated and give the count of number of times it is repeated
+
+sort filename | uniq -d     -> Displays onlythe repeated lines
