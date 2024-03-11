75b2af01be299a82e148a214732442affb70390d
jagankumar2001 <154983877+jagankumar2001@users.noreply.github.com>
2024-03-11 07:14:14 +0530

    Create grep_operations.py
    Grep and egrep options have been mentioned here

---
+++ grep_operations.py
@@ -0,0 +1,14 @@
+Syntax:
+        grep keyword filename
+
+Examples:
+
+grep -c keyword filename      -> Search for a keyword and count
+
+grep -i keyword filename      -> Search for a keyword by ignoring the case sensitive
+
+grep -n keyword filename       ->  Display the matched lines and the numbers
+
+grep -v keyword filename       -> Display everything but keyword
+
+grep -i "keyword1|keyword2" filename    -> for searching 2 keywords
